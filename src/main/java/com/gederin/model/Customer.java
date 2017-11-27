e891d97dd37309f531608ed17fb49d37f7b4e417
rgederin <ruslan.gederin@goeuro.com>
2017-11-27 21:14:30 +0200

    add customer model and ignore application test

---
+++ src/main/java/com/gederin/model/Customer.java
@@ -0,0 +1,25 @@
+package com.gederin.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Document
+@Data
+@ToString
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
