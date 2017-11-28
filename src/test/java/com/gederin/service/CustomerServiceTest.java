c662b3a894fe2e248b6d8e8c492bce89e17b6f84
rgederin <ruslan.gederin@goeuro.com>
2017-11-28 08:58:33 +0200

    add tests for fetchAllCustomers

--- src/test/java/com/gederin/service/CustomerServiceTest.java
+++ src/test/java/com/gederin/service/CustomerServiceTest.java
@@ -1,5 +1,7 @@
 package com.gederin.service;
 
+import com.google.common.collect.ImmutableList;
+
 import com.gederin.model.Customer;
 import com.gederin.repository.CustomerRepository;
 
@@ -10,11 +12,15 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CustomerServiceTest {
@@ -24,27 +30,46 @@ public class CustomerServiceTest {
 
     private CustomerService customerService;
 
+    private Customer customer;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         customerService = new CustomerService(customerRepository);
+        customer = new Customer("Ruslan Gederin", "21931298312");
     }
 
     @Test
-    public void shouldInvokeRepositorySaveMethod(){
-        customerService.saveCustomer(new Customer());
+    public void shouldInvokeRepositorySaveMethod() {
+        customerService.saveCustomer(customer);
 
         verify(customerRepository, times(1)).save(any());
     }
 
     @Test
-    public void shouldInvokeRepositorySaveMethodWithSameCustomer (){
+    public void shouldInvokeRepositorySaveMethodWithSameCustomer() {
         ArgumentCaptor<Customer> argumentCaptor = ArgumentCaptor.forClass(Customer.class);
-        Customer customer = new Customer("Ruslan Gederin", "21931298312");
 
         customerService.saveCustomer(customer);
 
         verify(customerRepository, times(1)).save(argumentCaptor.capture());
 
-        assertThat("Repository should save the same object as received", customer.equals(argumentCaptor.getValue()), equalTo(true));
+        assertThat(customer.equals(argumentCaptor.getValue()), equalTo(true));
+    }
+
+    @Test
+    public void shouldInvokedRepositoryFindAllWhenFetchAllCustomers() {
+        customerService.fetchAllCustomers();
+
+        verify(customerRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void shouldReturnListOfCustomerWhenFetchAllCustomers() {
+        List<Customer> customers = ImmutableList.of(customer, new Customer("RG", "33"));
+        when(customerRepository.findAll()).thenReturn(customers);
+
+        assertThat(customerService.fetchAllCustomers(), notNullValue());
+        assertThat(customerService.fetchAllCustomers().size(), equalTo(2));
+        assertThat(customerService.fetchAllCustomers().get(0), equalTo(customer));
     }
 }
